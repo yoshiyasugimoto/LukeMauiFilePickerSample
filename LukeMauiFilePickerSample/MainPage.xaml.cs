6197dedbc4634c2fb81c3b7fb9e7199409898515
yoshiyasugimoto <gabesaiti@gmail.com>
2025-08-11 00:13:13 +0900

    macosのfilepickerが動くことを確認

---
+++ LukeMauiFilePickerSample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LukeMauiFilePicker;
+
+namespace LukeMauiFilePickerSample;
+
+public partial class MainPage : ContentPage
+{
+	private readonly IFilePicker _filePicker;
+
+	public MainPage(IFilePicker filePicker)
+	{
+		_filePicker = filePicker;
+		InitializeComponent();
+	}
+
+	private async void OnUploadClicked(object? sender, EventArgs e)
+	{
+		try
+		{
+			var fileTypes = new Dictionary<DevicePlatform, IEnumerable<string>>
+			{
+				{ DevicePlatform.iOS, new[] { "public.item" } },
+				{ DevicePlatform.Android, new[] { "*/*" } },
+				{ DevicePlatform.MacCatalyst, new[] { "public.item" } },
+				{ DevicePlatform.WinUI, new[] { "*" } }
+			};
+	
+			var pickOptions = new PickOptions
+			{
+				FileTypes = new FilePickerFileType(fileTypes)
+			};
+	
+			var file = await _filePicker.PickAsync(pickOptions);
+
+			if (file != null)
+			{
+				UploadBtn.Text = $"Selected: {file.FileName}";
+				SemanticScreenReader.Announce($"File selected: {file.FileName}");
+				System.Diagnostics.Debug.WriteLine($"Picked fullpath: {file.FullPath}");
+				System.Diagnostics.Debug.WriteLine($"Picked file: {file.FileName}");
+			}
+		}
+		catch (Exception ex)
+		{
+			UploadBtn.Text = "Upload file";
+			await DisplayAlert("Error", $"Failed to pick file: {ex.Message}", "OK");
+		}
+	}
+}
---
+++ LukeMauiFilePickerSample/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LukeMauiFilePickerSample;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
